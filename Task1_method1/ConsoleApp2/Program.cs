d685d7fb8fa71d8492afbd17f8d64b9b45f2066b
GulgazSuleymanli <bd7tk5y26@code.edu.az>
2023-09-27 02:10:30 +0400

    Modified Algoritms_Tasks

---
+++ Task1_method1/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = -384;
+            int y = -18;
+            int z = 0;
+
+            if (x == y && y==z)
+            {
+                Console.WriteLine("Ededler beraberdir");
+            }
+            else
+            {
+                if (x>y)
+                {
+                    if(x>z)
+                    {
+                        Console.WriteLine("En boyuk eded x-dir");
+                    }
+                    else
+                    {
+                        Console.WriteLine("En boyuk eded z-dir");
+                    }
+                }
+                else
+                {
+                    if(y>z)
+                    {
+                        Console.WriteLine("En boyuk eded y-dir");
+                    }
+                    else
+                    {
+                        Console.WriteLine("En boyuk eded z-dir");
+                    }
+                }
+            }
+        }
+    }
+}
